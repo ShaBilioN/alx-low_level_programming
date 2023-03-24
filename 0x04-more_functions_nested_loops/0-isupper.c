0fca0a20c65cb89b4d1ef9808d74679e99ae732a
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-24 03:02:14 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
-    int c;
+#include "main.c"
 
-    scanf("%c",&c);
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+int _isupper(int c)
+{
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
@@ -9,3 +15,4 @@
 	{
 		return (0);
 	}
+}
