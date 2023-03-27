625c9aef6045bc5f7a31ce4d34cc092a62e45932
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-28 00:18:45 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != '\0')
+    {
+        putchar(*(str+count));
+        count++;
+    }
+    printf("\n");
+}
