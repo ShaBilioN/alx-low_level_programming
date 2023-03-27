ad260141c4332a571df461998d807806ccc30575
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-28 00:34:24 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,17 @@
  */
 void print_rev(char *s)
 {
-    int count = 0;
-    
-    while (*(s + count) != '\0')
-    {
-        count++;
-    }
-    
-    count--;
-    while (count >= 0)
-    {
-        putchar(*(s + count));
-        count--;
-    }
-    printf("\n");
+	int count = 0;
+	
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	count--;
+	while (count >= 0)
+	{
+		putchar(*(s + count));
+		count--;
+	}
+	printf("\n");
 }
