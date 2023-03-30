f45ae642ea358ee327e03647be4e136f2e28c35a
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-31 00:23:10 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,18 @@
  */
 char *_strcat(char *s1,char *s2)
 {
-	char *str;
 	int i = 0, count = 0;
 
 	while (*(s1 + i) != '\0')
 	{
 		i++;
 	}
+
 	while (*(s2 + count) != '\0')
 	{
 		*(s1 + i) = *(s2 + count);
 		i++;
 		count++;
 	}
-
 	return (s1);
 }
