32bca536b1fa3155e6350700d664633bf2567e6f
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-31 00:53:21 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,8 +4,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
     int i = 0;
 
-    for(;i < n && dest[i] != '\0'; i++)
+    for (;i < n && dest[i] != '\0'; i++)
         dest[i] = src[i];
-	
+    for (;i < n; i++)
+    {
+        dest[i] = '\0';
+    }
     return (dest);
 }
