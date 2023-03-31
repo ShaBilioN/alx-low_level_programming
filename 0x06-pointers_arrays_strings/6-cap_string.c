5210aa79ec4fec7cdb8d50f6c7e6751a54571862
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-31 02:25:59 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + i) == sep_op[count])
 			{
-				if(*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+				if((*(s + i + 1) >= 97) && (*(s + i + 1) <= 122))
 					*(s + i + 1) = *(s + i + 1) - 32;
 				break;
 			}
