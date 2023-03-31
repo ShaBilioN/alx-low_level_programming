e7046354339677a13f0f38f75d960ee6fd0123e4
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-03-31 02:19:14 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,15 @@ char *cap_string(char *s)
 {
     int i = 0,count;
     char sep_op[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
-	while (s[i] != '\0')
+
+	while (*(s + i) != '\0')
     {
         for (count = 0; count < 13; count++)
         {
-            if (s[i] == sep_op[count])
+            if (*(s + i) == sep_op[count])
             {
-                if(s[i + 1] >= 97 && s[i + 1] <= 122)
-                    s[i + 1] = s[i + 1] - 32;
+                if(*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+                    *(s + i + 1) = *(s + i + 1) - 32;
                 break;
             }
         }
