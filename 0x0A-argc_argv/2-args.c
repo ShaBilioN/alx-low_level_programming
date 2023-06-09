2cdfc264bd3763c2d098f6585c6b940d2ae57ad3
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-06-10 02:59:55 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,14 +7,9 @@
  */
 int main(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    if(argc >= 0)
-    {
-        for (i = 0; i < argc; i++)
-        {
-            printf("%s\n", argv[i]);
-        }
-    }
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
