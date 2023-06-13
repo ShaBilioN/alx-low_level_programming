b3e449d794aa8bae9051d76b4c6983354e4b50a5
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-06-14 01:03:17 +0300

    Create 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,11 +24,17 @@ char *str_concat(char *s1, char *s2)
 
 	str = (char *)malloc(sizeof(char) * size);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			str[i] = s1[i];
+	}
 
-	for (j = 0; s2[j] != '\0'; j++)
-		str[i + j] = s2[j];
+	if (s2 != NULL)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+			str[i + j] = s2[j];
+	}
 
 	return (str);
 }
