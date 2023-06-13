81e8d7cdc31a9806431dc3969b2887a18dbe87d9
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-06-14 01:59:23 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	gridout = (int *)malloc(sizeof(int) * height);
+	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
 		free(gridout);
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		gridout[i] = (int *)malloc(sizeof(int) * width);
+		gridout[i] = malloc(width * sizeof(int));
 		if (gridout[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
