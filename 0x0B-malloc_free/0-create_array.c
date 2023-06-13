6144a01490ff32978985bf503f1cb743e520f609
ShaBilioN <98710607+ShaBilioN@users.noreply.github.com>
2023-06-13 19:26:41 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,15 +13,12 @@ char *create_array(unsigned int size, char c)
     char *A;
 
     if (size <= 0)
-    {
         return (NULL);
-    }
+    
     A = malloc(sizeof(c) * size);
 
     for (i = 0; i < size; i++)
-    {
         A[i] = c;
-    }
 
     return (A);
 }
